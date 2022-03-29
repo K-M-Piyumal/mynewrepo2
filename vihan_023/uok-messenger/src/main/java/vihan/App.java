08ad72226e47cecc496827d696079445196c9896
Vihan Perera <87504524+Vihan-Perera@users.noreply.github.com>
2022-03-29 19:08:48 +0530

    add single message between client and server

--- vihan_023/uok-messenger/src/main/java/vihan/App.java
+++ vihan_023/uok-messenger/src/main/java/vihan/App.java
@@ -1,13 +1,9 @@
-package vihan;
+public class App{
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+     public static void main(String []args){
+        System.out.println("Hello World123");
+		no1 = 1;
+		no2 = 2;
+		no3 = 3;
+     }
 }
